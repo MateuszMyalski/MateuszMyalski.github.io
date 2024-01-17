e574c4c8914403b14fe14b8bbd6225f3da9f2eca
Mateusz Myalski <mateusz.myalski@gmail.com>
2024-01-17 11:47:11 +0100

    Fix RSS path

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,4 @@ from pelicanconf import *
 SITEURL = "https://mateuszmyalski.github.io/"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
-
 DELETE_OUTPUT_DIRECTORY = True
